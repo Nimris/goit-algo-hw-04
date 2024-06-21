ebf24f5bd5386a14f3bfd43265717f15be8a9c69
Stanislav <fenixmnio@gmail.com>
2024-06-21 11:23:10 +0300

    Update main.py

--- HW4-2/main.py
+++ HW4-2/main.py
@@ -1,3 +1,19 @@
-def get_cats_info(path: str) -> str:
-    pass
-#2
+def get_cats_info(path: str) -> list:
+    result = []
+    try:
+        with open(path, 'r', encoding = 'UTF-8') as file:
+            for line in file:
+                try:
+                    id, name, age = line.strip().split(',')
+                    result.append({"id": id, "name": name, "age": int(age)})
+                except ValueError:
+                    print(f"Skipping invalid line: {line}")
+            
+            print(result)
+            return result
+        
+    except FileNotFoundError:
+        print("Error 404")
+        return []
+        
+get_cats_info("cats.txt")
